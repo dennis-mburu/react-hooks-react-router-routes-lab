75a873a7684210ef4f14df772df72a4cb9056e45
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-21 22:16:58 +0300

    commit tree 421d795e1c8c6234c9c33a2ea050558bf214f12e

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors Page</h1>
+    <>
+    {actors.map(actors => {return(
+      <div key={actors.name}>
+        <h2>Name: {actors.name}</h2>
+        <ul>
+          {actors.movies.map((movie,index) => <li key={index}>{movie}</li>
+          )}
+        </ul>
+      </div>
+    )})}
+    </>
+  </div>;
 }
 
 export default Actors;
